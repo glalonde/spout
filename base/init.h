3665264f12497ab0b41a7642ffa740b905d22eca
Geoffrey Lalonde <lalondegeoffrey@gmail.com>
2019-03-27 12:27:56 -0700

    wip

--- base/init.h
+++ base/init.h
@@ -4,6 +4,7 @@
 void Init(int& argc, char** argv) {
   // Init glog
   google::InitGoogleLogging(argv[0]);
+  google::InstallFailureSignalHandler();
   // Parse cmd line flags
   gflags::ParseCommandLineFlags(&argc, &argv, true);
   // Use console output for logging rather than file.
---
+++ src/opengl.h
@@ -0,0 +1,7 @@
+#pragma once
+// clang-format off
+#include <glad/glad.h>
+// clang-format on
+#include <GL/glu.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_opengl.h>
