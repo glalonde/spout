dc44427917bf21c70715ad475211302f160a4e3f
Geoffrey Lalonde <lalondegeoffrey@gmail.com>
2019-04-04 10:07:06 -0700

    wip

--- src/bresenham_demo.cc
+++ src/bresenham_demo.cc
@@ -1,61 +1,115 @@
 #include <thread>
 
+#include "base/format.h"
 #include "base/init.h"
-#include "base/time.h"
-#include "new_spout/bresenham.h"
-#include "new_spout/controller_input.h"
-#include "new_spout/particle.h"
-#include "new_spout/sdl_container.h"
+#include "src/bresenham.h"
+#include "src/convert.h"
+#include "src/fonts/font_renderer.h"
+#include "src/image_viewer/animated_canvas.h"
+
+DEFINE_int32(num_particles, 100, "Number of particles");
+
+static constexpr uint8_t kWall = std::numeric_limits<uint8_t>::max();
+static const PixelType::RGBAU8 kWallColor = {0, 0, 255, 255};
+static const PixelType::RGBAU8 kParticleColor = {0, 255, 0, 255};
+static const PixelType::RGBAU8 kTrailColor = {0, 128, 0, 255};
+static const PixelType::RGBAU8 text_color =
+    Convert<PixelType::RGBAU8>(PixelType::RGBF64(1.0, 0.0, 0.0));
+
+void RenderEnvironment(const Image<uint8_t>& env,
+                       Image<PixelType::RGBAU8>* data) {
+  for (int c = 0; c < env.cols(); ++c) {
+    for (int r = 0; r < env.rows(); ++r) {
+      if (env(r, c) == kWall) {
+        (*data)(r, c) = kWallColor;
+      } else {
+        (*data)(r, c) = {0, 0, 0, 255};
+      }
+    }
+  }
+}
+
+void RenderParticle(const Vector2d& pos, Image<PixelType::RGBAU8>* data) {
+  // (x, y) -> (col, height - row)
+  Vector2i pos_i = pos.cast<int>();
+  pos_i[1] = data->rows() - pos_i[1] - 1;
+  (*data)(pos_i[1], pos_i[0]) = kParticleColor;
+}
+
+template <class T>
+void AddWalls(const T& wall_value, Image<T>* data) {
+  // Set left and right to walls
+  for (int r = 0; r < data->rows(); ++r) {
+    (*data)(r, 0) = wall_value;
+    (*data)(r, data->cols() - 1) = wall_value;
+  }
+  // Set top and bottom to walls
+  for (int c = 0; c < data->cols(); ++c) {
+    (*data)(0, c) = wall_value;
+    (*data)(data->rows() - 1, c) = wall_value;
+  }
+}
+
+void AddFpsText(double fps, const PixelType::RGBAU8& color,
+                Image<PixelType::RGBAU8>* data) {
+  std::string fps_string = FormatString("%.0f", fps);
+  RenderString(fps_string, {1, 1}, color, 1,
+               font_rendering::Justification::kLeft, data);
+}
+
+template <class T>
+void AddNoise(const T& wall_value, Image<T>* data) {}
 
 void Demo(int num_particles) {
-  const Vector2i grid_dims(100, 100);
-  const Vector2i screen_dims(1000, 1000);
-  PixelBuffer<uint32_t> buffer(grid_dims);
-  SDLContainer sdl(screen_dims, grid_dims);
-  ControllerInput input;
-  Eigen::AlignedBox<double, 5> particle_space;
+  // Set up canvas
+  const double kFps = 60.0;
+  const Vector2i window_dims(400, 400);
+  const Vector2i grid_dims = window_dims / 4;
+  AnimatedCanvas canvas(window_dims[0], window_dims[1], grid_dims[0],
+                        grid_dims[1], kFps);
+
+  // Set up environment
+  Image<uint8_t> environment(grid_dims[1], grid_dims[0]);
+  environment.setConstant(0);
+  AddWalls(kWall, &environment);
+
+  // Set up particles
+  AlignedBox<double, 4> particle_space;
   {
-    particle_space.max() << 50, 50, 30, 30, 10;
-    particle_space.min() << 50, 50, -30, -30, 10;
+    particle_space.max() << 50, 50, 30, 30;
+    particle_space.min() << 50, 50, -30, -30;
   }
-  std::vector<Particle<double>> particles(num_particles);
+  std::vector<Vector4d> particles(num_particles);
   for (int i = 0; i < num_particles; ++i) {
     particles[i] = particle_space.sample();
   }
 
-  buffer.Clear();
-  for (int r = 0; r < grid_dims.y(); ++r) {
-    buffer(r, 0) = kWall;
-    buffer(r, grid_dims.x() - 1) = kWall;
-  }
-  for (int c = 0; c < grid_dims.x(); ++c) {
-    buffer(0, c) = kWall;
-    buffer(grid_dims.y() - 1, c) = kWall;
-  }
 
   Vector2d pos;
   Vector2d vel;
-  double dt = 1.0 / 60.0;
+  double dt = ToSeconds<double>(FromHz(kFps));
   const double ddy = -9.81;
-  while (!input.quit) {
-    auto start = ClockType::now();
+
+  bool done = false;
+  auto* data = canvas.data();
+  while (!done) {
+    RenderEnvironment(environment, data);
     for (int i = 0; i < num_particles; ++i) {
       SubPixelBresenhamNormal(particles[i].segment<2>(0),
-                              particles[i].segment<2>(2), dt, &buffer, &pos,
-                              &vel);
+                              particles[i].segment<2>(2), dt, &environment,
+                              &pos, &vel);
       particles[i].segment<2>(0) = pos;
       particles[i].segment<2>(2) = vel;
       particles[i][3] += dt * ddy;
+      RenderParticle(pos, data);
     }
-
-    sdl.UpdateInput(&input);
-    sdl.Render(buffer);
-    std::this_thread::sleep_until(start + FromSeconds<double>(dt));
+    AddFpsText(canvas.fps(), text_color, data);
+    done = canvas.Tick().quit;
   }
 }
 
 int main(int argc, char** argv) {
   Init(argc, argv);
-  Demo(100);
+  Demo(FLAGS_num_particles);
   return 0;
 }
--- src/bresenham_test.cc
+++ src/bresenham_test.cc
@@ -1,13 +1,13 @@
 #include <thread>
 
 #include "base/googletest.h"
-#include "base/time.h"
 #include "src/bresenham.h"
 #include "src/image_viewer/animated_canvas.h"
 
 static constexpr uint8_t kWall = std::numeric_limits<uint8_t>::max();
 static const PixelType::RGBAU8 kWallColor = {0, 0, 255, 255};
 static const PixelType::RGBAU8 kParticleColor = {0, 255, 0, 255};
+static const PixelType::RGBAU8 kTrailColor = {0, 128, 0, 255};
 
 void RenderEnvironment(const Image<uint8_t>& env,
                        Image<PixelType::RGBAU8>* data) {
@@ -29,31 +29,37 @@ void RenderParticle(const Vector2d& pos, Image<PixelType::RGBAU8>* data) {
   (*data)(pos_i[1], pos_i[0]) = kParticleColor;
 }
 
+template <class T>
+void AddWalls(const T& wall_value, Image<T>* data) {
+  // Set left and right to walls
+  for (int r = 0; r < data->rows(); ++r) {
+    (*data)(r, 0) = wall_value;
+    (*data)(r, data->cols() - 1) = wall_value;
+  }
+  // Set top and bottom to walls
+  for (int c = 0; c < data->cols(); ++c) {
+    (*data)(0, c) = wall_value;
+    (*data)(data->rows() - 1, c) = wall_value;
+  }
+}
+
 GTEST_TEST(BresenhamTest, SmokeVis) {
+  const double kFps = 60.0;
   // Width, height (not rows, cols)
   const Vector2i window_dims(400, 400);
   const Vector2i grid_dims = window_dims / 4;
   AnimatedCanvas canvas(window_dims[0], window_dims[1], grid_dims[0],
-                        grid_dims[1], 60.0);
+                        grid_dims[1], kFps);
 
   Image<uint8_t> environment(grid_dims[1], grid_dims[0]);
   environment.setConstant(0);
-  const uint8_t kWall = std::numeric_limits<uint8_t>::max();
-  // Set left and right to walls
-  for (int r = 0; r < grid_dims.y(); ++r) {
-    environment(r, 0) = kWall;
-    environment(r, grid_dims.x() - 1) = kWall;
-  }
-  // Set top and bottom to walls
-  for (int c = 0; c < grid_dims.x(); ++c) {
-    environment(0, c) = kWall;
-    environment(grid_dims.y() - 1, c) = kWall;
-  }
+  AddWalls(kWall, &environment);
 
   // Create 1 particle
   Vector2d pos(50, 20);
-  Vector2d vel = Vector2d(7, 6.9).normalized() * 150;
-  double dt = 1.0 / 300.0;
+  Vector2d vel = Vector2d(7, 6.9).normalized() * 10.0;
+  double dt = 1.0 / kFps;
+  std::vector<Vector2i> trails;
 
   bool done = false;
   auto* data = canvas.data();
