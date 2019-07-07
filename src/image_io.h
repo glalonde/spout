e510b8dcab20b63442e726a6511caa6b1adb3c5f
Geoffrey Lalonde <lalondegeoffrey@gmail.com>
2019-07-06 20:56:54 -0700

    image write

--- src/image_io.h
+++ src/image_io.h
@@ -5,3 +5,5 @@
 #include "src/image.h"
 
 std::optional<Image<PixelType::RGBAU8>> ReadImage(const std::string& path);
+void WriteImage(const Image<PixelType::RGBAU8>& image,
+                const std::string& path);
