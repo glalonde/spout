4520ac901f723293ba9cd9296665f191f399836a
Geoffrey Lalonde <lalondegeoffrey@gmail.com>
2020-02-17 12:16:45 -0800

    Music error handling

--- rust/src/music_player.rs
+++ rust/src/music_player.rs
@@ -1,5 +1,5 @@
 use lazy_static::lazy_static;
-use log::{error, trace};
+use log::{error, info, trace};
 
 // Singleton music player...
 lazy_static! {
@@ -66,12 +66,10 @@ impl<'a> MusicPlayer<'a> {
             })
             .filter_map(Result::ok)
             .collect();
-
         if files.len() <= 0 {
-            return Err(String::from("No songs to play"));
+            return Err("No music files".to_string());
         }
         player.library = files;
-
         Ok(player)
     }
 
@@ -96,7 +94,7 @@ impl<'a> MusicPlayer<'a> {
         if sdl2::mixer::Music::is_paused() {
             // If we were already playing something.
             sdl2::mixer::Music::resume();
-        } else {
+        } else if self.library.len() > 0 {
             if let Err(err) = self.library[self.current_track].play(1) {
                 error!("Error playing file: {}", err);
             }
@@ -111,8 +109,10 @@ impl<'a> MusicPlayer<'a> {
     fn handle_next_track(&mut self) {
         trace!("Next track");
         let num_songs = self.library.len();
-        self.current_track = (self.current_track + 1) % num_songs;
-        self.handle_play();
+        if num_songs > 0 {
+            self.current_track = (self.current_track + 1) % num_songs;
+            self.handle_play();
+        }
     }
 
     fn handle_track_finished(&mut self) {
@@ -123,19 +123,57 @@ impl<'a> MusicPlayer<'a> {
 
 pub struct MusicThread {
     command_channel: std::sync::mpsc::Sender<MusicPlayerCommand>,
-    _thread: std::thread::JoinHandle<()>,
+    _thread: Option<std::thread::JoinHandle<()>>,
 }
 impl MusicThread {
-    // TODO pass through failed player init result
     pub fn init() -> Self {
         let (tx, rx) = std::sync::mpsc::channel::<MusicPlayerCommand>();
-        MusicThread {
-            command_channel: tx,
-            _thread: std::thread::spawn(move || {
-                let mut player =
-                    MusicPlayer::init(std::path::Path::new(LIBRARY_DIR.flag), rx).unwrap();
-                player.run();
-            }),
+
+        let (tx_create, rx_create) = std::sync::mpsc::channel::<Result<(), String>>();
+        // Try to start the music thread.
+        let maybe_thread = std::thread::Builder::new()
+            .name("music_thread".to_string())
+            .spawn(move || {
+                let maybe_player = MusicPlayer::init(std::path::Path::new(LIBRARY_DIR.flag), rx);
+                match maybe_player {
+                    Err(msg) => {
+                        let _ = tx_create.send(Err(msg));
+                    }
+                    Ok(mut player) => {
+                        let _ = tx_create.send(Ok(()));
+                        player.run();
+                    }
+                }
+            });
+
+        match maybe_thread {
+            Err(err) => {
+                // If we couldn't even start the thread, bail out.
+                error!("Error starting music thread: {}", err);
+                MusicThread {
+                    command_channel: tx,
+                    _thread: None,
+                }
+            }
+            Ok(thread) => match rx_create.recv().unwrap() {
+                Err(msg) => {
+                    // If we started the thread, but couldn't initialize the player, join the thread
+                    // and bail out.
+                    error!("Error initializing music player: {}", msg);
+                    let _ = thread.join();
+                    MusicThread {
+                        command_channel: tx,
+                        _thread: None,
+                    }
+                }
+                Ok(_) => {
+                    info!("Started music player.");
+                    MusicThread {
+                        command_channel: tx,
+                        _thread: Some(thread),
+                    }
+                }
+            },
         }
     }
     pub fn play(&self) {
