75e0758c20c532808f9d2ab9b0eb1bba93dee7ee
Geoffrey Lalonde <lalondegeoffrey@gmail.com>
2021-12-29 00:47:10 -0800

    wip

---
+++ src/camera.rs
@@ -0,0 +1,40 @@
+
+#[path = "../examples/framework.rs"]
+mod framework;
+pub struct Camera {
+    pub screen_size: (u32, u32),
+    // Camera in cylindrical coordinates.
+    pub phi: f32,
+    pub radius: f32,
+    pub height: f32,
+}
+
+impl Camera {
+    /// Outputs 4x4 projection matrix and 4x4 view matrix
+    pub fn to_uniform_data(&self) -> [f32; 16 * 2] {
+        let aspect = self.screen_size.0 as f32 / self.screen_size.1 as f32;
+
+        // pixel_pose_camera
+        let mx_projection = cgmath::perspective(cgmath::Deg(45f32), aspect, 1.0, 50.0);
+
+        let cam_pos = cgmath::Point3::new(
+            self.phi.cos() * self.radius,
+            self.phi.sin() * self.radius,
+            self.height,
+        );
+
+        // camera_pose_world 
+        let mx_view = cgmath::Matrix4::look_at_rh(
+            cam_pos,
+            cgmath::Point3::new(0f32, 0.0, 0.0),
+            cgmath::Vector3::unit_z(),
+        );
+        let proj = framework::OPENGL_TO_WGPU_MATRIX * mx_projection;
+        let view = framework::OPENGL_TO_WGPU_MATRIX * mx_view;
+
+        let mut raw = [0f32; 16 * 2];
+        raw[..16].copy_from_slice(&AsRef::<[f32; 16]>::as_ref(&proj)[..]);
+        raw[16..32].copy_from_slice(&AsRef::<[f32; 16]>::as_ref(&view)[..]);
+        raw
+    }
+}
--- src/textured_model.wgsl
+++ src/textured_model.wgsl
@@ -10,19 +10,26 @@ struct ViewData {
 [[group(0), binding(0)]]
 var<uniform> view_data: ViewData;
 
+struct ModelData {
+    pose: mat4x4<f32>;
+};
+[[group(1), binding(2)]]
+var<uniform> model_data: ModelData;
+
 [[stage(vertex)]]
 fn vs_main([[location(0)]] pos: vec4<f32>, [[location(1)]] tex_coord: vec2<f32>) -> VertexOutput {
     var out: VertexOutput;
     out.tex_coord = tex_coord;
-    // pixel_pose_camera * camera_pose_model 
-    out.position = view_data.projection * view_data.view * pos;
+    // pixel_pose_camera * camera_pose_world * world_pose_model * model_pose_vertex
+    out.position = view_data.projection * view_data.view * model_data.pose * pos;
     return out;
 }
 
-[[group(0), binding(1)]]
+[[group(1), binding(0)]]
 var r_color: texture_2d<f32>;
-[[group(0), binding(2)]]
+[[group(1), binding(1)]]
 var r_sampler: sampler;
+
 
 [[stage(fragment)]]
 fn fs_main(in: VertexOutput) -> [[location(0)]] vec4<f32> {
