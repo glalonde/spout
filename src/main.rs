e5e4f1d11b094bd735fcad1669415c5ce77a988e
Geoffrey Lalonde <lalondegeoffrey@gmail.com>
2021-12-29 00:47:10 -0800

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,120 +1,50 @@
+mod camera;
 #[path = "../examples/framework.rs"]
 mod framework;
+mod game_params;
+mod textured_quad;
 
-use bytemuck::{Pod, Zeroable};
-use cgmath::SquareMatrix;
-use std::{borrow::Cow, mem, num::NonZeroU32};
+use log::error;
+use std::{borrow::Cow, mem};
 use wgpu::util::DeviceExt;
 
-const TEXTURE_FORMAT: wgpu::TextureFormat = wgpu::TextureFormat::Rgba8UnormSrgb;
 
-#[repr(C)]
-#[derive(Clone, Copy, Pod, Zeroable)]
-struct Vertex {
-    _pos: [f32; 4],
-    _tex_coord: [f32; 2],
+gflags::define! {
+    --config: &str = "game_config.toml"
 }
 
-fn vertex(pos: [i8; 3], tc: [i8; 2]) -> Vertex {
-    Vertex {
-        _pos: [pos[0] as f32, pos[1] as f32, pos[2] as f32, 1.0],
-        _tex_coord: [tc[0] as f32, tc[1] as f32],
-    }
-}
-
-fn create_texels(size: usize, cx: f32, cy: f32) -> Vec<u8> {
-    use std::iter;
-
-    (0..size * size)
-        .flat_map(|id| {
-            // get high five for recognizing this ;)
-            let mut x = 4.0 * (id % size) as f32 / (size - 1) as f32 - 2.0;
-            let mut y = 2.0 * (id / size) as f32 / (size - 1) as f32 - 1.0;
-            let mut count = 0;
-            while count < 0xFF && x * x + y * y < 4.0 {
-                let old_x = x;
-                x = x * x - y * y + cx;
-                y = 2.0 * old_x * y + cy;
-                count += 1;
-            }
-            iter::once(0xFF - (count * 2) as u8)
-                .chain(iter::once(0xFF - (count * 5) as u8))
-                .chain(iter::once(0xFF - (count * 13) as u8))
-                .chain(iter::once(std::u8::MAX))
-        })
-        .collect()
-}
-
-fn create_vertices() -> (Vec<Vertex>, Vec<u16>) {
-    let vertex_data = [
-        // top (0, 0, 1)
-        vertex([-1, -1, 0], [0, 1]),
-        vertex([1, -1, 0], [1, 1]),
-        vertex([1, 1, 0], [1, 0]),
-        vertex([-1, 1, 0], [0, 0]),
-    ];
-    let index_data: &[u16] = &[
-        0, 1, 2, 2, 3, 0, // top
-    ];
-    (vertex_data.to_vec(), index_data.to_vec())
-}
-
-struct Camera {
-    screen_size: (u32, u32),
-    // Camera in cylindrical coordinates.
-    phi: f32,
-    radius: f32,
-    height: f32,
-}
-
-impl Camera {
-    fn to_uniform_data(&self) -> [f32; 16 * 2] {
-        let aspect = self.screen_size.0 as f32 / self.screen_size.1 as f32;
-
-        // pixel_pose_camera
-        let mx_projection = cgmath::perspective(cgmath::Deg(45f32), aspect, 1.0, 50.0);
-
-        let cam_pos = cgmath::Point3::new(
-            self.phi.cos() * self.radius,
-            self.phi.sin() * self.radius,
-            self.height,
-        );
-
-        // camera_pose_world 
-        let mx_view = cgmath::Matrix4::look_at_rh(
-            cam_pos,
-            cgmath::Point3::new(0f32, 0.0, 0.0),
-            cgmath::Vector3::unit_z(),
-        );
-        let proj = framework::OPENGL_TO_WGPU_MATRIX * mx_projection;
-        let view = framework::OPENGL_TO_WGPU_MATRIX * mx_view;
-
-        let mut raw = [0f32; 16 * 2];
-        raw[..16].copy_from_slice(&AsRef::<[f32; 16]>::as_ref(&proj)[..]);
-        raw[16..32].copy_from_slice(&AsRef::<[f32; 16]>::as_ref(&view)[..]);
-        raw
-    }
-}
 /*
 TODO Render into the preloaded texture.
  */
 
 struct Example {
-    // Geometry for the canvas.
-    vertex_buf: wgpu::Buffer,
-    index_buf: wgpu::Buffer,
-    index_count: usize,
+    camera: camera::Camera,
+    camera_bind_group: wgpu::BindGroup,
+    camera_uniform_buf: wgpu::Buffer,
 
-    bind_group: wgpu::BindGroup,
-    uniform_buf: wgpu::Buffer,
     draw_pipeline: wgpu::RenderPipeline,
-    camera: Camera,
+
+    model: textured_quad::TexturedQuad,
+
     frame_num: i64,
     staging_belt: wgpu::util::StagingBelt,
 }
 
 impl Example {
+    fn read_config_from_file(path: &str) -> anyhow::Result<game_params::GameParams> {
+        let params = std::fs::read_to_string(path)?.parse()?;
+        Ok(params)
+    }
 
+    fn get_game_config() -> game_params::GameParams {
+        match Example::read_config_from_file(CONFIG.flag) {
+            Ok(params) => params,
+            Err(e) => {
+                error!("Failed to parse config file({}): {:?}", CONFIG.flag, e);
+                game_params::GameParams::default()
+            }
+        }
+    }
 }
 
 impl framework::Example for Example {
@@ -131,83 +61,23 @@ impl framework::Example for Example {
         let mut init_encoder =
             device.create_command_encoder(&wgpu::CommandEncoderDescriptor { label: None });
 
-        // Create the texture
-        let size: u32 = 1 << 9;
-        let texels = create_texels(size as usize, -0.8, 0.156);
-        let texture_extent = wgpu::Extent3d {
-            width: size as u32,
-            height: size as u32,
-            depth_or_array_layers: 1,
-        };
-        let texture = device.create_texture(&wgpu::TextureDescriptor {
-            size: texture_extent,
-            mip_level_count: 1,
-            sample_count: 1,
-            dimension: wgpu::TextureDimension::D2,
-            format: TEXTURE_FORMAT,
-            usage: wgpu::TextureUsages::TEXTURE_BINDING | wgpu::TextureUsages::COPY_DST,
-            label: None,
-        });
-        let texture_view = texture.create_view(&wgpu::TextureViewDescriptor::default());
-        //Note: we could use queue.write_texture instead, and this is what other
-        // examples do, but here we want to show another way to do this.
-        let temp_buf = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
-            label: Some("Temporary Buffer"),
-            contents: texels.as_slice(),
-            usage: wgpu::BufferUsages::COPY_SRC,
-        });
-        init_encoder.copy_buffer_to_texture(
-            wgpu::ImageCopyBuffer {
-                buffer: &temp_buf,
-                layout: wgpu::ImageDataLayout {
-                    offset: 0,
-                    bytes_per_row: Some(NonZeroU32::new(4 * size).unwrap()),
-                    rows_per_image: None,
-                },
-            },
-            texture.as_image_copy(),
-            texture_extent,
-        );
+        let game_params = Example::get_game_config();
 
-        // Create the vertex and index buffers
-        let vertex_size = mem::size_of::<Vertex>();
-        let (vertex_data, index_data) = create_vertices();
-        let vertex_buf = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
-            label: Some("Vertex buffer"),
-            contents: bytemuck::cast_slice(&vertex_data),
-            usage: wgpu::BufferUsages::VERTEX,
-        });
-        let index_buf = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
-            label: Some("Index buffer"),
-            contents: bytemuck::cast_slice(&index_data),
-            usage: wgpu::BufferUsages::INDEX,
-        });
-
-        // Create other resources
-        let sampler = device.create_sampler(&wgpu::SamplerDescriptor {
-            label: None,
-            address_mode_u: wgpu::AddressMode::Repeat,
-            address_mode_v: wgpu::AddressMode::Repeat,
-            address_mode_w: wgpu::AddressMode::Repeat,
-            mag_filter: wgpu::FilterMode::Linear,
-            min_filter: wgpu::FilterMode::Linear,
-            mipmap_filter: wgpu::FilterMode::Linear,
-            ..Default::default()
-        });
-
-        let camera = Camera {
+        let camera = camera::Camera {
             screen_size: (config.width, config.height),
             radius: 5.0,
             phi: 0.0,
             height: 3.0,
         };
         let raw_uniforms = camera.to_uniform_data();
-        let uniform_buf = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+        let camera_uniform_buf = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
             label: Some("Uniform Buffer"),
             contents: bytemuck::cast_slice(&raw_uniforms),
             usage: wgpu::BufferUsages::UNIFORM | wgpu::BufferUsages::COPY_DST,
         });
 
+        // Create the vertex and index buffers
+        let vertex_size = mem::size_of::<textured_quad::Vertex>();
         let vertex_buffers = [wgpu::VertexBufferLayout {
             array_stride: vertex_size as wgpu::BufferAddress,
             step_mode: wgpu::VertexStepMode::Vertex,
@@ -230,7 +100,7 @@ impl framework::Example for Example {
         // Create the render pipeline
         let shader = device.create_shader_module(&wgpu::ShaderModuleDescriptor {
             label: None,
-            source: wgpu::ShaderSource::Wgsl(Cow::Borrowed(include_str!("draw.wgsl"))),
+            source: wgpu::ShaderSource::Wgsl(Cow::Borrowed(include_str!("textured_model.wgsl"))),
         });
 
         let draw_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
@@ -256,41 +126,36 @@ impl framework::Example for Example {
         });
 
         // Create bind group
-        let bind_group_layout = draw_pipeline.get_bind_group_layout(0);
-        let bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
-            layout: &bind_group_layout,
-            entries: &[
-                wgpu::BindGroupEntry {
-                    binding: 0,
-                    resource: uniform_buf.as_entire_binding(),
-                },
-                wgpu::BindGroupEntry {
-                    binding: 1,
-                    resource: wgpu::BindingResource::TextureView(&texture_view),
-                },
-                wgpu::BindGroupEntry {
-                    binding: 2,
-                    resource: wgpu::BindingResource::Sampler(&sampler),
-                },
-            ],
+        let camera_bind_group_layout = draw_pipeline.get_bind_group_layout(0);
+        let camera_bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
+            layout: &camera_bind_group_layout,
+            entries: &[wgpu::BindGroupEntry {
+                binding: 0,
+                resource: camera_uniform_buf.as_entire_binding(),
+            }],
             label: None,
         });
+
+        let textured_quad = textured_quad::TexturedQuad::init(
+            device,
+            draw_pipeline.get_bind_group_layout(1),
+            &mut init_encoder,
+        );
 
         queue.submit(Some(init_encoder.finish()));
 
         Example {
-            vertex_buf,
-            index_buf,
-            index_count: index_data.len(),
-            bind_group,
-            uniform_buf,
-            draw_pipeline,
-            camera: Camera {
+            camera: camera::Camera {
                 screen_size: (config.width, config.height),
                 radius: 5.0,
                 phi: 0.0,
                 height: 3.0,
             },
+            camera_bind_group,
+            camera_uniform_buf,
+            draw_pipeline,
+            model: textured_quad,
+
             frame_num: 0,
             staging_belt: wgpu::util::StagingBelt::new(0x100),
         }
@@ -325,7 +190,7 @@ impl framework::Example for Example {
             self.staging_belt
                 .write_buffer(
                     &mut encoder,
-                    &self.uniform_buf,
+                    &self.camera_uniform_buf,
                     0,
                     wgpu::BufferSize::new((raw_uniforms.len() * 4) as wgpu::BufferAddress).unwrap(),
                     device,
@@ -342,23 +207,26 @@ impl framework::Example for Example {
                 b: 0.3,
                 a: 1.0,
             };
-            let mut rpass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
-                label: None,
-                color_attachments: &[wgpu::RenderPassColorAttachment {
-                    view,
-                    resolve_target: None,
-                    ops: wgpu::Operations {
-                        load: wgpu::LoadOp::Clear(clear_color),
-                        store: true,
-                    },
-                }],
-                depth_stencil_attachment: None,
-            });
-            rpass.set_pipeline(&self.draw_pipeline);
-            rpass.set_bind_group(0, &self.bind_group, &[]);
-            rpass.set_index_buffer(self.index_buf.slice(..), wgpu::IndexFormat::Uint16);
-            rpass.set_vertex_buffer(0, self.vertex_buf.slice(..));
-            rpass.draw_indexed(0..self.index_count as u32, 0, 0..1);
+            {
+                let mut rpass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                    label: None,
+                    color_attachments: &[wgpu::RenderPassColorAttachment {
+                        view,
+                        resolve_target: None,
+                        ops: wgpu::Operations {
+                            load: wgpu::LoadOp::Clear(clear_color),
+                            store: true,
+                        },
+                    }],
+                    depth_stencil_attachment: None,
+                });
+                rpass.set_pipeline(&self.draw_pipeline);
+
+                // Bind camera data.
+                rpass.set_bind_group(0, &self.camera_bind_group, &[]);
+
+                self.model.render(&mut rpass);
+            }
         }
 
         self.frame_num += 1;
---
+++ src/textured_quad.rs
@@ -0,0 +1,187 @@
+use bytemuck::{Pod, Zeroable};
+use wgpu::util::DeviceExt;
+
+#[repr(C)]
+#[derive(Clone, Copy, Pod, Zeroable)]
+pub struct Vertex {
+    _pos: [f32; 4],
+    _tex_coord: [f32; 2],
+}
+
+fn vertex(pos: [i8; 3], tc: [i8; 2]) -> Vertex {
+    Vertex {
+        _pos: [pos[0] as f32, pos[1] as f32, pos[2] as f32, 1.0],
+        _tex_coord: [tc[0] as f32, tc[1] as f32],
+    }
+}
+
+fn create_texels(size: usize, cx: f32, cy: f32) -> Vec<u8> {
+    use std::iter;
+
+    (0..size * size)
+        .flat_map(|id| {
+            // get high five for recognizing this ;)
+            let mut x = 4.0 * (id % size) as f32 / (size - 1) as f32 - 2.0;
+            let mut y = 2.0 * (id / size) as f32 / (size - 1) as f32 - 1.0;
+            let mut count = 0;
+            while count < 0xFF && x * x + y * y < 4.0 {
+                let old_x = x;
+                x = x * x - y * y + cx;
+                y = 2.0 * old_x * y + cy;
+                count += 1;
+            }
+            iter::once(0xFF - (count * 2) as u8)
+                .chain(iter::once(0xFF - (count * 5) as u8))
+                .chain(iter::once(0xFF - (count * 13) as u8))
+                .chain(iter::once(std::u8::MAX))
+        })
+        .collect()
+}
+
+fn create_vertices() -> (Vec<Vertex>, Vec<u16>) {
+    let vertex_data = [
+        // top (0, 0, 1)
+        vertex([-1, -1, 0], [0, 1]),
+        vertex([1, -1, 0], [1, 1]),
+        vertex([1, 1, 0], [1, 0]),
+        vertex([-1, 1, 0], [0, 0]),
+    ];
+    let index_data: &[u16] = &[
+        0, 1, 2, 2, 3, 0, // top
+    ];
+    (vertex_data.to_vec(), index_data.to_vec())
+}
+
+pub struct TexturedQuad {
+    // Geometry for the canvas.
+    vertex_buf: wgpu::Buffer,
+    index_buf: wgpu::Buffer,
+    index_count: usize,
+    bind_group: wgpu::BindGroup,
+}
+
+impl TexturedQuad {
+    pub fn init(
+        device: &wgpu::Device,
+        bind_group_layout: wgpu::BindGroupLayout,
+        init_encoder: &mut wgpu::CommandEncoder,
+    ) -> Self {
+        // Create the texture
+        let size: u32 = 1 << 9;
+        let texels = create_texels(size as usize, -0.8, 0.156);
+        let texture_extent = wgpu::Extent3d {
+            width: size as u32,
+            height: size as u32,
+            depth_or_array_layers: 1,
+        };
+        let texture = device.create_texture(&wgpu::TextureDescriptor {
+            size: texture_extent,
+            mip_level_count: 1,
+            sample_count: 1,
+            dimension: wgpu::TextureDimension::D2,
+            format: wgpu::TextureFormat::Rgba8UnormSrgb,
+            usage: wgpu::TextureUsages::TEXTURE_BINDING | wgpu::TextureUsages::COPY_DST,
+            label: None,
+        });
+        let texture_view = texture.create_view(&wgpu::TextureViewDescriptor::default());
+        //Note: we could use queue.write_texture instead, and this is what other
+        // examples do, but here we want to show another way to do this.
+        let temp_buf = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+            label: Some("Temporary Buffer"),
+            contents: texels.as_slice(),
+            usage: wgpu::BufferUsages::COPY_SRC,
+        });
+        init_encoder.copy_buffer_to_texture(
+            wgpu::ImageCopyBuffer {
+                buffer: &temp_buf,
+                layout: wgpu::ImageDataLayout {
+                    offset: 0,
+                    bytes_per_row: Some(std::num::NonZeroU32::new(4 * size).unwrap()),
+                    rows_per_image: None,
+                },
+            },
+            texture.as_image_copy(),
+            texture_extent,
+        );
+
+        // Create the vertex and index buffers
+        let (vertex_data, index_data) = create_vertices();
+        let vertex_buf = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+            label: Some("Vertex buffer"),
+            contents: bytemuck::cast_slice(&vertex_data),
+            usage: wgpu::BufferUsages::VERTEX,
+        });
+        let index_buf = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+            label: Some("Index buffer"),
+            contents: bytemuck::cast_slice(&index_data),
+            usage: wgpu::BufferUsages::INDEX,
+        });
+
+        // Create other resources
+        let sampler = device.create_sampler(&wgpu::SamplerDescriptor {
+            label: None,
+            address_mode_u: wgpu::AddressMode::Repeat,
+            address_mode_v: wgpu::AddressMode::Repeat,
+            address_mode_w: wgpu::AddressMode::Repeat,
+            mag_filter: wgpu::FilterMode::Linear,
+            min_filter: wgpu::FilterMode::Linear,
+            mipmap_filter: wgpu::FilterMode::Linear,
+            ..Default::default()
+        });
+
+        // Model pose uniform
+        let model_pose = cgmath::Matrix4::from_translation(cgmath::Vector3 {
+            x: 0.0,
+            y: 0.0,
+            z: 0.0,
+        });
+        let pose_uniform_buf = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+            label: Some("Uniform Buffer"),
+            contents: bytemuck::cast_slice(&AsRef::<[f32; 16]>::as_ref(&model_pose)[..]),
+            usage: wgpu::BufferUsages::UNIFORM | wgpu::BufferUsages::COPY_DST,
+        });
+
+        // Create bind group for the non-camera inputs.
+        let bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
+            layout: &bind_group_layout,
+            entries: &[
+                wgpu::BindGroupEntry {
+                    binding: 0,
+                    resource: wgpu::BindingResource::TextureView(&texture_view),
+                },
+                wgpu::BindGroupEntry {
+                    binding: 1,
+                    resource: wgpu::BindingResource::Sampler(&sampler),
+                },
+                wgpu::BindGroupEntry {
+                    binding: 2,
+                    resource: pose_uniform_buf.as_entire_binding(),
+                },
+            ],
+            label: None,
+        });
+
+        TexturedQuad {
+            vertex_buf,
+            index_buf,
+            index_count: index_data.len(),
+            bind_group,
+        }
+    }
+
+    pub fn render<'a>(&'a self, rpass: &mut wgpu::RenderPass<'a>) {
+        // Assume camera bind group (0) has already been set by outer context.
+
+        // Set the texture data
+        rpass.set_bind_group(1, &self.bind_group, &[]);
+
+        // Set the mesh data
+        rpass.set_index_buffer(self.index_buf.slice(..), wgpu::IndexFormat::Uint16);
+        rpass.set_vertex_buffer(0, self.vertex_buf.slice(..));
+
+        // Pose the geometry pose data
+        // rpass.set_bind_group(2, ...)
+
+        rpass.draw_indexed(0..self.index_count as u32, 0, 0..1);
+    }
+}
