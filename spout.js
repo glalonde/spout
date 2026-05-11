06382f4525fd5cdb5a174ed5bfb80bd844d437a5
glalonde <glalonde@users.noreply.github.com>
2026-05-11 04:22:55 +0000

    deploy: 015c47b30d0a1089e37d29536a11b02a16bcb8c4

--- spout.js
+++ spout.js
@@ -1708,7 +1708,7 @@ function __wbg_get_imports() {
             return ret;
         },
         __wbindgen_cast_000000000000000d: function(arg0, arg1) {
-            // Cast intrinsic for `Closure(Closure { dtor_idx: 471, function: Function { arguments: [NamedExternref("TouchEvent")], shim_idx: 472, ret: Unit, inner_ret: Some(Unit) }, mutable: true }) -> Externref`.
+            // Cast intrinsic for `Closure(Closure { dtor_idx: 504, function: Function { arguments: [NamedExternref("TouchEvent")], shim_idx: 505, ret: Unit, inner_ret: Some(Unit) }, mutable: true }) -> Externref`.
             const ret = makeMutClosure(arg0, arg1, wasm.wasm_bindgen__closure__destroy__ha5ae33a03d7ea89c, wasm_bindgen__convert__closures_____invoke__had9802450bfa31d8);
             return ret;
         },
